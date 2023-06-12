a04848e64e7aa8256b7dc8e5fa631cc4a07beb48
JuanjoCieri <juin@hotmail.com.ar>
2023-06-11 23:01:23 -0300

    push

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Main () {
+    return (
+        <main className="">
+            <h1 className="text-5xl text-white font-bold">Juan José Cieri</h1>
+            <h2 className="text-2xl font-semibold">Full Stack <p className="text-yellow-500">Developer</p></h2>
+        </main>
+    )
+}
